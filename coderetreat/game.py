b11dbe90a0f1bb32457cf4df28171864f147dea3
Sergio Soto Núñez <scots4ever@gmail.com>
2017-11-18 20:15:38 +0100

    Code and test refactor

--- coderetreat/game.py
+++ coderetreat/game.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import random
+
+
 DEAD = 0
 ALIVE = 1
 
@@ -10,27 +12,16 @@ class Location(object):
         self.x = x
         self.y = y
 
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
-
-
-    def is_alive(self):
-        return self.get_status() == ALIVE
-
 
 class Cell(object):
 
-    def __init__(self, status, debug=False, x=None, y=None, status):
+    def __init__(self, status, debug=False, x=None, y=None):
         self.status = status
         if not debug:
             self.location = Location(
                 random.randint(1, 10), random.randint(1, 10))
         else:
             self.location = Location(x, y)
-
 
     def is_alive(self):
         return self.status == ALIVE
--- tests/testing.py
+++ tests/testing.py
@@ -1,31 +1,38 @@
 import unittest
 
-from coderetreat.game import Universe, Cell, DEAD, ALIVE
+from coderetreat.game import Universe, Cell, Location, DEAD, ALIVE
 
 
 class TestUniverse(unittest.TestCase):
 
-    def test_check_universe_creation(self):
+    def setUp(self):
+        super(TestUniverse, self)
+        self.initial_alive_cells = 5
+        self.universe = Universe(cells_alive=self.initial_alive_cells)
+
+    def test_check_universe_alive_cells(self):
         universe = Universe(cells_alive=3)
         assert len(universe.get_alive_cells()) == 3
 
-    def test_if_cell_is_alive(self):
-        universe = Universe(cells_alive=3)
-        for cell in universe.get_alive_cells():
+    def test_all_cells_alive_right(self):
+        for cell in self.universe.get_alive_cells():
             assert cell.is_alive()
 
-    def test_cells_location_two_dimensions(self):
-        universe = Universe(cells_alive=2)
-        for cell in  universe.get_alive_cells():
-            location = cell.get_location()
-            assert isinstance(location.get_x(), int)
-            assert isinstance(location.get_y(), int)
+
+class TestLocation(unittest.TestCase):
+
+    def test_cells_location_x_and_y(self):
+        location = Location(x=1, y=7)
+        assert location.x == 1
+        assert location.y == 7
+
 
 class TestCell(unittest.TestCase):
 
     def test_if_cell_is_alive(self):
         cell = Cell(status=ALIVE)
-        assert cell.is_alive() == ALIVE
+        assert cell.is_alive()
+
 
 if __name__ == '__main__':
     unittest.main()
