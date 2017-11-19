0b1057042abcb471ae9f6dd504a1b55a6551a243
Sergio Soto Núñez <scots4ever@gmail.com>
2017-11-19 21:23:51 +0100

    Added all behaviour with tests.
    Four initial rules has been implemented but not tested.

--- coderetreat/game.py
+++ coderetreat/game.py
@@ -5,6 +5,12 @@ import random
 DEAD = 0
 ALIVE = 1
 
+MAX_LOCATION_VALUE = 30
+
+
+class DuplicatedLocationCells(Exception):
+    pass
+
 
 class Location(object):
 
@@ -12,31 +18,138 @@ class Location(object):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return '{}, {}'.format(self.x, self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
 
 class Cell(object):
 
-    def __init__(self, status, debug=False, x=None, y=None):
+    def __init__(self, status=ALIVE, x=None, y=None):
         self.status = status
-        if not debug:
+        if not x and not y:
             self.location = Location(
-                random.randint(1, 10), random.randint(1, 10))
+                random.randint(0, MAX_LOCATION_VALUE),
+                random.randint(0, MAX_LOCATION_VALUE))
+        elif not x or not y:
+            raise ValueError(
+                'Parameters x and y should appear two or not of both')
         else:
             self.location = Location(x, y)
+
+    def __str__(self):
+        if self.status:
+            status = 'ALIVE'
+        else:
+            status = 'DEAD'
+
+        return 'Cell {}: ({})'.format(
+            status, self.location
+        )
+
+    def __eq__(self, other):
+        return self.location == other.location and self.status == other.status
 
     def is_alive(self):
         return self.status == ALIVE
 
-    def get_location(self):
-        return self.location
-
 
 class Universe(object):
 
-    def __init__(self, cells_alive):
-        self.number_cells_alive = cells_alive
+    def __init__(self, cells_alive=None):
+        self.cells = dict()
+        if cells_alive:
+            for i in range(cells_alive):
+                cell = Cell(status=ALIVE)
+                self.add_cell(cell)
+
+    def __str__(self):
+        return str([
+            '{}'.format(cell) for cell in self.cells.values()
+        ])
 
     def get_alive_cells(self):
-        cells_alive = []
-        for i in range(self.number_cells_alive):
-            cells_alive.append(Cell(ALIVE))
-        return cells_alive
+        return [
+            cell for cell in self.cells.values() if cell.is_alive()
+        ]
+
+    def add_cell(self, cell):
+        if self.cells.get(cell.location):
+            raise DuplicatedLocationCells('Duplicated cell: {}'.format(cell))
+        self.cells[cell.location] = cell
+
+    def add_cells(self, cells):
+        for cell in cells:
+            self.add_cell(cell)
+
+    def get_neighbours(self, cell):
+        neighbours = []
+        for location in Universe.neighbours_locations(cell):
+            neighbour = self.cells.get(location)
+            if neighbour:
+                neighbours.append(neighbour)
+        return neighbours
+
+    def get_alive_neighbours(self, cell):
+        return [cell for cell in self.get_neighbours(cell)
+                if cell.is_alive()]
+
+    @staticmethod
+    def __validate_neighbour_location(i, j, location):
+        result = True
+        if location.x == i and location.y == j:
+            result = False
+        elif i < 0 or j < 0:
+            result = False
+        elif i > MAX_LOCATION_VALUE or j > MAX_LOCATION_VALUE:
+            result = False
+        return result
+
+    @staticmethod
+    def neighbours_locations(cell):
+        locations = []
+        for i in range(cell.location.x - 1, cell.location.x + 2):
+            for j in range(cell.location.y-1, cell.location.y + 2):
+                if Universe.__validate_neighbour_location(i, j, cell.location):
+                    locations.append(Location(x=i, y=j))
+        return locations
+
+    def rule_1(self):
+        """
+        An alive cell in contact with less than two cells will die
+
+        :return:
+        """
+        for cell in self.get_alive_cells():
+            if len(self.get_neighbours()) >= 2:
+                cell.status = DEAD
+
+    def rule_2(self):
+        """
+        An alive cell in contact with two or three alive cells will live
+        """
+        for cell in self.get_alive_cells():
+            if len(self.get_alive_neighbours(cell)) in (2, 3):
+                cell.status = ALIVE
+
+    def rule_3(self):
+        """
+        An alive cell in contact with more than three cells will die
+        """
+        for cell in self.get_alive_cells():
+            if len(self.get_alive_neighbours(cell)) > 3:
+                cell.status = DEAD
+
+    def rule_4(self):
+        """
+        If a dead cell come in contact with three or more alive cells, will come
+        alive again
+        :return:
+        """
+        for cell in self.get_alive_neighbours()
+
--- tests/testing.py
+++ tests/testing.py
@@ -1,6 +1,7 @@
 import unittest
 
-from coderetreat.game import Universe, Cell, Location, DEAD, ALIVE
+from coderetreat.game import (Universe, Cell, Location, DEAD, ALIVE,
+                              DuplicatedLocationCells)
 
 
 class TestUniverse(unittest.TestCase):
@@ -18,6 +19,70 @@ class TestUniverse(unittest.TestCase):
         for cell in self.universe.get_alive_cells():
             assert cell.is_alive()
 
+    def test_cell_creation_with_location(self):
+        universe = Universe()
+        cell_test_1 = Cell(status=ALIVE, x=3, y=3)
+        universe.add_cell(cell_test_1)
+        universe_cells = universe.get_alive_cells()
+        assert len(universe_cells) == 1
+        assert universe_cells[0] == cell_test_1
+
+    def test_cell_add_with_listOfCells(self):
+        universe = Universe()
+        cells = [
+            Cell(status=ALIVE, x=3, y=3),
+            Cell(status=ALIVE, x=2, y=3), ]
+        universe.add_cells(cells)
+        assert (len(universe.get_alive_cells()) == 2)
+
+    def test_cell_add_with_listOfCells_duplicatedLocations(self):
+        universe = Universe()
+        cells = [
+            Cell(status=ALIVE, x=2, y=2),
+            Cell(status=ALIVE, x=2, y=2), ]
+        with self.assertRaises(DuplicatedLocationCells):
+            universe.add_cells(cells)
+
+    def test_cell_neighbour(self):
+        universe = Universe()
+        cell_1 = Cell(status=ALIVE, x=3, y=3)
+        cell_2 = Cell(status=ALIVE, x=2, y=3)
+        universe.add_cells([cell_1, cell_2])
+        neighbours = universe.get_neighbours(cell_1)
+        assert len(neighbours) == 1
+        assert neighbours[0] == cell_2
+
+    def test_universe_neighbours_index_calculation_len(self):
+        neighbour_locations = Universe.neighbours_locations(Cell(x=3, y=3))
+        assert len(neighbour_locations) == 8
+        assert Location(x=2, y=2) in neighbour_locations
+        assert Location(x=2, y=3) in neighbour_locations
+        assert Location(x=2, y=4) in neighbour_locations
+        assert Location(x=3, y=2) in neighbour_locations
+        assert Location(x=3, y=4) in neighbour_locations
+        assert Location(x=4, y=2) in neighbour_locations
+        assert Location(x=4, y=3) in neighbour_locations
+        assert Location(x=4, y=4) in neighbour_locations
+
+    def test_universe_get_alive_neigbours(self):
+        """
+                  2    3    4
+              2        A    D
+              3        A
+              4   A
+        """
+        universe = Universe()
+        cell_1 = Cell(status=ALIVE, x=2, y=3)
+        cell_2 = Cell(status=DEAD, x=2, y=4)
+        cell_3 = Cell(status=ALIVE, x=3, y=3)
+        cell_4 = Cell(status=ALIVE, x=4, y=2)
+        universe.add_cells([cell_1, cell_2, cell_3, cell_4])
+        alive_neighbours = universe.get_alive_neighbours(cell_3)
+        assert len(alive_neighbours) == 2
+        assert cell_1 in alive_neighbours
+        assert cell_2 not in alive_neighbours
+        assert cell_4 in alive_neighbours
+
 
 class TestLocation(unittest.TestCase):
 
@@ -32,6 +97,28 @@ class TestCell(unittest.TestCase):
     def test_if_cell_is_alive(self):
         cell = Cell(status=ALIVE)
         assert cell.is_alive()
+
+    def test_if_cell_is_dead(self):
+        cell = Cell(status=DEAD)
+        assert not cell.is_alive()
+
+    def test_cell_location_withXandY(self):
+        cell = Cell(status=ALIVE, x=8, y=30)
+        assert cell.location.x == 8
+        assert cell.location.y == 30
+
+    def test_cell_location_raise_exception_yIsNone(self):
+        with self.assertRaises(ValueError):
+            Cell(status=DEAD, x=30)
+
+    def test_cell_location_raise_exception_xIsNone(self):
+        with self.assertRaises(ValueError):
+            Cell(status=DEAD, y=92)
+
+    def test_cell_location_withRandomValues(self):
+        cell = Cell(status=DEAD)
+        assert isinstance(cell.location.x, int)
+        assert isinstance(cell.location.y, int)
 
 
 if __name__ == '__main__':
